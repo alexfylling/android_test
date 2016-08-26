7a50551795c0fbdc37d25dfa6f78a987ea981ace
alexfylling <alexfylling@gmail.com>
2016-08-26 12:20:46 +0200

    First

---
+++ app/src/main/java/com/example/alexfylling/android_test/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.alexfylling.android_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton sendButton = (ImageButton)findViewById(R.id.imageButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hey My World", Toast.LENGTH_SHORT).show();
+                
+                
+                
+                //// TODO: 26.08.2016  
+            }
+        });
+    }
+}
